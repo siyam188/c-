423555003e4df66045310f1bf8fc1742bcc159da
Siyam Mia <siyambd188@gmail.com>
2025-11-17 21:02:18 +0600

    Create celsius_to_fahrenheit.cpp

---
+++ celsius_to_fahrenheit.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   double farn,cels;
+   cout<<"Enter a celsius:";
+   cin>> cels;
+  farn = 1.8*cels+32;
+  cout<< " Fahrenheit = "<<farn;
+    return 0;
+}
