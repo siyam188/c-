1abf537d311ad8bcc6935917d60f28e2e2b1a10d
Siyam Mia <siyambd188@gmail.com>
2025-11-17 21:10:37 +0600

    Add increment and decrement operations in C++

---
+++ increment_decrement.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int x= 10;
+   int y = ++x;
+   cout <<x<<endl;
+  cout <<y<<endl;
+   int a = --x;
+    cout<<a<<endl;
+   int b = x++;
+    cout<<b<<endl;
+    int z = x--;
+     cout<<z<<endl;
+
+
+    return 0;
+}
