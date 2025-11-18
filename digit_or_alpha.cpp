3a4f9ccfe77695302c7125528af23a9bac5ece23
Siyam Mia <siyambd188@gmail.com>
2025-11-18 08:22:27 +0600

    Implement digit or alpha character check

---
+++ digit_or_alpha.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char X;
+    cin >> X;
+
+    if (X >= '0' && X <= '9') {
+        cout << "IS DIGIT";
+    } 
+    else {
+        cout << "ALPHA" << endl;
+        if (X >= 'A' && X <= 'Z') {
+            cout << "IS CAPITAL";
+        }
+        else if (X >= 'a' && X <= 'z') {
+            cout << "IS SMALL";
+        }
+    }
+
+    return 0;
+}
