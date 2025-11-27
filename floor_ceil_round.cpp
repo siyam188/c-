f539845749bd25b056998cea88e148aceed87f8b
Siyam Mia <siyambd188@gmail.com>
2025-11-27 21:22:19 +0600

    Create floor_ceil_round.cpp

---
+++ floor_ceil_round.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+
+    cout << "floor " << A << " / " << B << " = " << floor(A / B) << "\n";
+    cout << "ceil " << A << " / " << B << " = " << ceil(A / B) << "\n";
+    cout << "round " << A << " / " << B << " = " << round(A / B) << "\n";
+
+    return 0;
+}
